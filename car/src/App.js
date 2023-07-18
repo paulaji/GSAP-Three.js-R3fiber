707e453d10b0f186758f97f7f051df665487a5f0
paulaji <paulajiparayil123@gmail.com>
2023-07-18 21:55:24 +0530

    added a basic mesh

--- car/src/App.js
+++ car/src/App.js
@@ -2,9 +2,22 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 function CarShow() {
-  return null;
+  return (
+    <>
+      {/* OrbitControls helps us to move the camera around a fixed point, [0, 0.35, 0] basically represents center of the screen with a slight elevation in the y-axis, maxPolarAngle defines the tilt from which we view */}
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      {/* mesh object is basically used to create a 3d cube in the scene, boxGeometry is the dimensions of the cube */}
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+    </>
+  );
 }
 
 function App() {
