21f24b9f33754c1e0c309389d32f0f93fb14abed
paulaji <paulajiparayil123@gmail.com>
2023-07-18 21:25:56 +0530

    added new project and made some changes to prev

---
+++ car/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+// react now uses concurrent mode entry process to load things faster for the users to see
+// createRoot() helps in the same
+// it takes a DOM element as arguement, here document.getElementById("root") and this represents the new entry point of the app
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
